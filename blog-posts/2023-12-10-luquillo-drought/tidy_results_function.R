d0679422b653b9159cba5cea77bbe51c34ce4e2f
bbarajas429 <bricavii2012@gmail.com>
2024-06-21 22:02:39 +0000

    Finished blog corrections

--- blog-posts/2023-12-10-luquillo-drought/tidy_results_function.R
+++ blog-posts/2023-12-10-luquillo-drought/tidy_results_function.R
@@ -5,9 +5,9 @@ tidy_results_fun <- function(model, data){
   tidy <- broom::tidy(model)
   summary <- broom::glance(model)
   
-  # combine desired results
-  tidy$adj_r_squared <- c(summary$adj.r.squared, NA)
-  tidy$r_sqaured <- c(summary$r.squared, NA)
+  # combine desired results (keeping R/R^2 only once)
+  tidy$adj_r_squared <- ifelse(1:nrow(tidy) == 1, summary$adj.r.squared, NA)
+  tidy$r_squared <- ifelse(1:nrow(tidy) == 1, summary$r.squared, NA)
   
   # save results to environment
   assign(paste0(tolower(unique(data$species)), "_model"), tidy, envir = .GlobalEnv)
